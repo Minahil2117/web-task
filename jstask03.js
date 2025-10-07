04ad76e60303bb6e25dfb689809829f1ddcc877b
Minahil Fatima <meerubawan471@gmail.com>
2025-10-07 17:25:09 +0500

    Add files via upload

---
+++ jstask03.js
@@ -0,0 +1,26 @@
+//array creation
+let employees=["Asad","Minu","Sadia","Sarmad",true,false,6,4,2];
+console.log(employees);
+employees.push(Yes);
+employees.push(NO);
+console.log(employees);
+employees.unshift("Amber");
+employees.unshift("Laiba");
+console.log(employees);
+//another array
+let customers=["Ali","Basit","Umer",7,9,5,true,false];
+console.log(customers);
+customers.pop(true);
+customers.pop(false);
+console.log(customers);
+customers.shift("Ali");
+customers.shift("Basit");
+console.log(customers);
+console.log(customers[4]);
+console.log(customers[2]);
+console.log(customers);
+
+
+
+
+
