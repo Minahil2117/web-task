693204307b9eae154e690a67fb30463a42b0d5b6
Minahil Fatima <meerubawan471@gmail.com>
2025-10-11 12:57:45 +0500

    Add files via upload

---
+++ jstask04.js
@@ -0,0 +1,14 @@
+// Task 1
+function getEmail(){
+    let useremail=prompt("Enter your Email");
+    alert(`Your Email is ${useremail}`);
+}
+getEmail();
+// Task 2
+const stylingbox = () => {
+  let box = document.querySelector("#box");
+  box.style.width = "400px";
+  box.style.fontSize = "20px"; 
+  box.style.color = "green";
+  box.style.backgroundColor = "lightblue";
+};
